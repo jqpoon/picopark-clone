77294fc3f60f938141c2c390152a9f6795678974
Jia Qi Poon <poonjiaqi@gmail.com>
2022-12-25 11:30:33 +0000

    Fix webpack config to at least load something

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  mode: 'development',
+
   plugins: [
     new HtmlWebpackPlugin({
       inject: "body",
-      template: "./src/",
+      template: "./client/index.html",
       filename: "index.html",
       minify: {
         removeComments: true,
@@ -12,13 +21,13 @@ module.exports = {
       },
     }),
 
-    // new HtmlWebpackPlugin({
-    //   template: "./src/contact.html",
-    //   filename: "contact.html",
-    //   minify: {
-    //     removeComments: true,
-    //     collapseWhitespace: true,
-    //   },
-    // }),
+    new HtmlWebpackPlugin({
+      template: "./src/contact.html",
+      filename: "contact.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
   ],
 };
